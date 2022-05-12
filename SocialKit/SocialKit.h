e9fe2a2a923961398006b6b34e10f05e27dd8791
psychehose <psychehose@gmail.com>
2022-05-12 10:58:57 +0900

    Social Kit Kakao Test

---
+++ SocialKit/SocialKit.h
@@ -0,0 +1,18 @@
+//
+//  SocialKit.h
+//  SocialKit
+//
+//  Created by KIM HOSE on 2022/05/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SocialKit.
+FOUNDATION_EXPORT double SocialKitVersionNumber;
+
+//! Project version string for SocialKit.
+FOUNDATION_EXPORT const unsigned char SocialKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocialKit/PublicHeader.h>
+
+
